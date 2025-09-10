1abf08af80361e0020653c68abf4b1fd1071ab08
Felipe <felipepg@uniplaclages.edu.br>
2025-09-10 20:04:41 -0300

    Separated the responsability of the download to another class

---
+++ Exceptions/DownloadException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeVideoDownloader.Exceptions
+{
+    public class DownloadException : Exception
+    {
+        public override string Message => "Error while downloading";
+    }
+}
